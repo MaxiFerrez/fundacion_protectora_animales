925ebca68f6dbba4804c9312b76d7701c258db9f
MaxiFerrez <maxiferrez@gmail.com>
2022-12-02 21:33:24 -0300

    Implementacion Del envio de mail

---
+++ app/Mail/FundacionMail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class FundacionMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    //public $subject="Fundacion Animal";
+    //public $nombre;
+    public function __construct()
+    {
+        //$this->nombre=$nombre;
+    }
+    public function build(){
+        return $this->view('mail.fundacion');
+        //return $this->view('mail.fundacion');
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Fundacion Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+           // view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\MensajesController;
 use GuzzleHttp\Middleware;
+use App\Mail\FundacionMail;
+use Illuminate\Support\Facades\Mail;
 
 Route::post('articulos/importar',[ArticulosController::class,'ImportarExcel'])->Middleware('auth')->name('importarArticulos');
 Route::get('articulos/exportar',[ArticulosController::class,'ExportarExcel'])->Middleware('auth')->name('exportarArticulos');
@@ -16,3 +18,11 @@ Route::view('home' , 'admin');
 Route::resource('articulos',ArticulosController::class)->middleware('auth');
 
 Route::resource('mensajes',MensajesController::class);
+
+Route::get('/mail',  function(){
+    $correo = new FundacionMail;
+    Mail::to('maxiferrez@gmail.com')->send($correo);
+
+    return "Mensaje Enviado";
+
+});
