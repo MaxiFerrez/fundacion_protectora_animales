b6970c682cf1e98f84f91d3c3e71965cc1bb3909
MaxiFerrez <maxiferrez@gmail.com>
2022-12-10 20:32:41 -0300

    Cambio de img home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,23 @@
     <link rel="stylesheet" href="{{asset('app.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
-<body>
-    <a href="{{route('login')}}"><button class="btn btn-danger">Login</button></a>
-    
+
+<body class="bg-image" 
+style="background-image: url('https://cdn.pixabay.com/photo/2021/06/01/07/05/paw-6300799_960_720.png');
+       height: 100vh">
+    {{-- <a href="{{route('login')}}"><button class="btn btn-danger">Login</button></a> --}}
+    @include('seccion.nav')
     <header>
         <h1>Fundacion protectora de animales 2022</h1>
     </header>
-    
+    <div lass="bg-success p-2 text-dark bg-opacity-25">
     <section>   
-        <img src="{{asset('portada.jpg')}}" alt="">
+        <img src="{{asset('perro.png')}}" alt="">
     </section>
     <section>
         @include('mensajes.create')
     </section>
+</div>
     <footer>
         <p>San Juan Tec 2022</p>
     </footer>
